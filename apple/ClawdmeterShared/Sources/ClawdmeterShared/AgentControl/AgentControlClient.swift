6f2f0cd3d6155d9fbf6ed9bb7c726ae28ce67168
darshanbathija <40279780+darshanbathija@users.noreply.github.com>
2026-05-22 12:35:35 +0700

    v0.22.15: fix Linux shared crypto build

--- apple/ClawdmeterShared/Sources/ClawdmeterShared/AgentControl/AgentControlClient.swift
+++ apple/ClawdmeterShared/Sources/ClawdmeterShared/AgentControl/AgentControlClient.swift
@@ -1,5 +1,9 @@
 import Foundation
+#if canImport(CryptoKit)
 import CryptoKit
+#else
+import Crypto
+#endif
 import OSLog
 
 private let clientLogger = Logger(subsystem: "com.clawdmeter.client", category: "AgentControlClient")
